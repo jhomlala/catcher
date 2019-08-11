b0de0fe76d8d55da8b4e59a1c10fb1ed0f9c7e07
Jakub <jhomlala@gmail.com>
2019-08-11 11:11:16 +0200

    Added discord handler

---
+++ example/lib/discord_handler_example.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:catcher/catcher_plugin.dart';
+
+main() {
+  CatcherOptions debugOptions = CatcherOptions(SilentReportMode(), [
+    DiscordHandler(
+        "https://discordapp.com/api/webhooks/610021484378718208/6pDaGiRjZMbzv6ouldDKhulEzoBQb0dJNDiz3lc3HwafzfzZKB18K7ERgshxMaj_BVGY",
+        enableDeviceParameters: true,
+        enableApplicationParameters: true,
+        enableCustomParameters: true,
+        enableStackTrace: true,
+        printLogs: true),
+    //ConsoleHandler()
+  ]);
+  CatcherOptions releaseOptions = CatcherOptions(NotificationReportMode(), [
+    EmailManualHandler(["recipient@email.com"])
+  ]);
+
+  Catcher(MyApp(), debugConfig: debugOptions, releaseConfig: releaseOptions);
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: Catcher.navigatorKey,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: ChildWidget()),
+    );
+  }
+}
+
+class ChildWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: FlatButton(
+            child: Text("Generate error"), onPressed: () => generateError()));
+  }
+
+  generateError() async {
+    Catcher.sendTestException();
+  }
+}
---
+++ lib/handlers/discord_handler.dart
@@ -0,0 +1,114 @@
+import 'package:catcher/handlers/report_handler.dart';
+import 'package:catcher/model/report.dart';
+import 'package:catcher/utils/catcher_utils.dart';
+import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
+
+class DiscordHandler extends ReportHandler {
+  final Dio _dio = Dio();
+  final Logger _logger = Logger("DiscordHandler");
+
+  final String webhookUrl;
+
+  final bool printLogs;
+  final bool enableDeviceParameters;
+  final bool enableApplicationParameters;
+  final bool enableStackTrace;
+  final bool enableCustomParameters;
+
+  DiscordHandler(
+    this.webhookUrl, {
+    this.printLogs = false,
+    this.enableDeviceParameters = false,
+    this.enableApplicationParameters = false,
+    this.enableStackTrace = false,
+    this.enableCustomParameters = false,
+  }) {
+    assert(webhookUrl != null, "Webhook must be specified");
+  }
+
+  @override
+  Future<bool> handle(Report report) async {
+    if (!(await CatcherUtils.isInternetConnectionAvailable())) {
+      _printLog("No internet connection available");
+      return false;
+    }
+
+    String message = _setupMessage(report);
+    List<String> messages = _setupMessages(message);
+
+    for (var value in messages) {
+      bool result = await _sendContent(value);
+      if (!result) {
+        return result;
+      }
+    }
+
+    return true;
+  }
+
+  List<String> _setupMessages(String message) {
+    List<String> messages = List();
+    int splits = (message.length / 2000).ceil();
+    int messageLength = message.length;
+    for (int index = 0; index < splits; index++) {
+      int startIndex = index * 2000;
+      int endIndex = startIndex + 2000;
+      if (endIndex > messageLength) {
+        endIndex = messageLength;
+      }
+      messages.add(message.substring(startIndex, endIndex));
+    }
+    return messages;
+  }
+
+  String _setupMessage(Report report){
+    StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.write("**Error:**\n${report.error}\n\n");
+    if (enableStackTrace) {
+      stringBuffer.write("**Stack trace:**\n${report.stackTrace}\n\n");
+    }
+    if (enableDeviceParameters && report.deviceParameters.length > 0) {
+      stringBuffer.write("**Device parameters:**\n");
+      for (var entry in report.deviceParameters.entries) {
+        stringBuffer.write("${entry.key}: ${entry.value}\n");
+      }
+      stringBuffer.write("\n\n");
+    }
+
+    if (enableApplicationParameters &&
+        report.applicationParameters.length > 0) {
+      stringBuffer.write("**Application parameters:**\n");
+      for (var entry in report.applicationParameters.entries) {
+        stringBuffer.write("${entry.key}: ${entry.value}\n");
+      }
+      stringBuffer.write("\n\n");
+    }
+
+    if (enableCustomParameters && report.customParameters.length > 0) {
+      stringBuffer.write("**Custom parameters:**\n");
+      for (var entry in report.customParameters.entries) {
+        stringBuffer.write("${entry.key}: ${entry.value}\n");
+      }
+      stringBuffer.write("\n\n");
+    }
+    return stringBuffer.toString();
+  }
+
+  Future<bool> _sendContent(String content) async {
+    var data = {
+      "content": content,
+    };
+    _printLog("Sending request to Discord server...");
+    Response response = await _dio.post(webhookUrl, data: data);
+    _printLog(
+        "Server responded with code: ${response.statusCode} and message: ${response.statusMessage}");
+    return response.statusCode >= 200 && response.statusCode < 300;
+  }
+
+  _printLog(String log) {
+    if (printLogs) {
+      _logger.info(log);
+    }
+  }
+}
