590c5d439605ebec4c60db09ad0a65d351f5c2bd
Dlani Mendes <dlanileonardo@gmail.com>
2019-12-12 23:19:23 -0300

    Upgrade to use v2 embedding FlutterActivity
    https://flutter.dev/docs/development/packages-and-plugins/plugin-api-migration
    https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects#add-to-app-migration

--- android/src/main/java/com/jhomlala/catcher/CatcherPlugin.java
+++ android/src/main/java/com/jhomlala/catcher/CatcherPlugin.java
@@ -1,18 +1,54 @@
 package com.jhomlala.catcher;
 
+import android.app.Activity;
+import androidx.lifecycle.Lifecycle;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.Result;
+
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** CatcherPlugin */
-public class CatcherPlugin implements MethodCallHandler {
-  /** Plugin registration. */
+public class CatcherPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
+  private static final String TAG = "CatcherPlugin";
+
+  // These are null when not using v2 embedding.
+  private MethodChannel channel;
+  private Lifecycle lifecycle;
+
+  private Activity activity;
+
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "catcher");
     channel.setMethodCallHandler(new CatcherPlugin());
   }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {}
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+    lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding);
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromActivity() {}
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {}
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {}
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
