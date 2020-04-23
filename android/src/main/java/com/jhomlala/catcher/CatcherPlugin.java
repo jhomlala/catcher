c97e9399d5a4dc43c1b425125949f76a26b29b1e
Jakub <jhomlala@gmail.com>
2020-04-23 21:04:23 +0200

    Fixed android plugin

--- android/src/main/java/com/jhomlala/catcher/CatcherPlugin.java
+++ android/src/main/java/com/jhomlala/catcher/CatcherPlugin.java
@@ -1,61 +1,41 @@
 package com.jhomlala.catcher;
 
-import android.app.Activity;
-import androidx.lifecycle.Lifecycle;
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.Result;
+import android.content.Context;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** CatcherPlugin */
-public class CatcherPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
-  private static final String TAG = "CatcherPlugin";
+public class CatcherPlugin implements FlutterPlugin {
 
-  // These are null when not using v2 embedding.
-  private MethodChannel channel;
-  private Lifecycle lifecycle;
+    private MethodChannel methodChannel;
+    private EventChannel eventChannel;
 
-  private Activity activity;
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "catcher");
-    channel.setMethodCallHandler(new CatcherPlugin());
-  }
-
-  @Override
-  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {}
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
-
-  @Override
-  public void onAttachedToActivity(ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-    lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding);
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onDetachedFromActivity() {}
-
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {}
-
-  @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {}
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    public static void registerWith(Registrar registrar) {
+        CatcherPlugin catcherPlugin = new CatcherPlugin();
+        catcherPlugin.setupChannels(registrar.messenger(), registrar.context());
     }
-  }
+
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        setupChannels(binding.getBinaryMessenger(), binding.getApplicationContext());
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        teardownChannels();
+    }
+
+
+    private void setupChannels(BinaryMessenger messenger, Context context) {
+        methodChannel = new MethodChannel(messenger, "catcher");
+        eventChannel = new EventChannel(messenger, "catcher_event");
+    }
+
+    private void teardownChannels() {
+        methodChannel = null;
+        eventChannel = null;
+    }
 }
