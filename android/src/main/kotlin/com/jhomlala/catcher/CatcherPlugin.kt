a043cf19424505e758f3bf07093e962d559d800f
Jakub  Homlala <j.homlala@bsgroup.eu>
2021-03-10 22:43:33 +0100

    Re-added platforms

--- android/src/main/kotlin/com/jhomlala/catcher/CatcherPlugin.kt
+++ android/src/main/kotlin/com/jhomlala/catcher/CatcherPlugin.kt
@@ -10,8 +10,8 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** CatcherPlugin */
-class CatcherPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
+
+class CatcherPlugin: FlutterPlugin, MethodCallHandler {  /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
